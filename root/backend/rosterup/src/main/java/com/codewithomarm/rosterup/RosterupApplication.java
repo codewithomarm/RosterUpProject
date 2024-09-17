a86d7dd7b224f8e243ba2f6edd485cce15f0f751
codewithomarm <codewithomarm@gmail.com>
2024-09-16 23:11:06 -0500

    back - add initial spring boot project

---
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/RosterupApplication.java
@@ -0,0 +1,13 @@
+package com.codewithomarm.rosterup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterupApplication.class, args);
+	}
+
+}
---
+++ root/backend/rosterup/src/test/java/com/codewithomarm/rosterup/RosterupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithomarm.rosterup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
