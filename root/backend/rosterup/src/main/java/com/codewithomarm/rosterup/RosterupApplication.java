e8a80ca49da852336bf5d22d0e71ba124c629cd6
codewithomarm <codewithomarm@gmail.com>
2024-09-17 08:59:45 -0500

    back - added logger to main class

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/RosterupApplication.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/RosterupApplication.java
@@ -1,13 +1,19 @@
 package com.codewithomarm.rosterup;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RosterupApplication {
+	private static final Logger logger = LoggerFactory.getLogger(
+			RosterupApplication.class
+	);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RosterupApplication.class, args);
+		logger.info("RosterupApplication started");
 	}
 
 }
