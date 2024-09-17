f46f3f109a198d9a2469882cf78fa7ba83a8cc05
codewithomarm <codewithomarm@gmail.com>
2024-09-17 16:42:06 -0500

    back - added Account Repository Interface

---
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.codewithomarm.rosterup.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
