a3ff5455c094cbc45a18338a9ccd5d8b738dddf9
codewithomarm <codewithomarm@gmail.com>
2024-09-18 08:55:29 -0500

    back - added account OneToMany relationship + getters and setters

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/Tenant.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/Tenant.java
@@ -2,20 +2,25 @@ package com.codewithomarm.rosterup.domain;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Tenant {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(name = "subdomain", nullable = false, length = 50, unique = true)
+    @Column(nullable = false, length = 50, unique = true)
     private String subdomain;
 
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tenant")
+    private List<Account> accounts;
 
     public Tenant() {
     }
@@ -52,5 +57,13 @@ public class Tenant {
 
     public void setActive(Boolean active) {
         isActive = active;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
     }
 }
