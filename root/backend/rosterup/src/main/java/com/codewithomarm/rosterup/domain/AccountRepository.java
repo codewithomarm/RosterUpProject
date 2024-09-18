6d312745f13739eec1297aaf65535f2e2172afb2
codewithomarm <codewithomarm@gmail.com>
2024-09-18 10:02:58 -0500

    back - fixed findAllActive and findAllInactive methods by adding @Query property

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/AccountRepository.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.codewithomarm.rosterup.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,10 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByName(String name);
 
     // Fetch only active accounts
+    @Query("SELECT a FROM Account a WHERE a.isActive = true")
     List<Account> findAllActive();
 
     // Fetch only inactive accounts
+    @Query("SELECT a FROM Account a WHERE a.isActive = false")
     List<Account> findAllInactive();
 }
