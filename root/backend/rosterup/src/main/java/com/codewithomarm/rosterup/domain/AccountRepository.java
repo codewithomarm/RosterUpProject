5970190802a2635e88de85eb27d61c675e7d927a
codewithomarm <codewithomarm@gmail.com>
2024-09-17 21:04:34 -0500

    back - Modified Account Repository

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/AccountRepository.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/domain/AccountRepository.java
@@ -2,5 +2,16 @@ package com.codewithomarm.rosterup.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    // Fetch account by name
+    Optional<Account> findByName(String name);
+
+    // Fetch only active accounts
+    List<Account> findAllActive();
+
+    // Fetch only inactive accounts
+    List<Account> findallInactive();
 }
