0623f25efea33528e612e0f04e801fae940ea7db
codewithomarm <codewithomarm@gmail.com>
2024-09-21 12:06:03 -0500

    back - added serializable + custom methods to handle exceptions

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/exceptions/TenantNotFoundException.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/exceptions/TenantNotFoundException.java
@@ -1,7 +1,16 @@
 package com.codewithomarm.rosterup.exceptions;
 
+import java.io.Serial;
+
 public class TenantNotFoundException extends RuntimeException {
-    public TenantNotFoundException(String message) {
-        super(message);
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public TenantNotFoundException(Long tenantId) {
+        super("Tenant not found with id: " + tenantId);
+    }
+
+    public TenantNotFoundException(String tenantSubdomain){
+        super("Tenant not found with subdomain: " + tenantSubdomain);
     }
 }
