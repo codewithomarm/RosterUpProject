9cf5ade908b32308e712e93c544d6e2551d44efa
codewithomarm <codewithomarm@gmail.com>
2024-09-26 10:42:52 -0500

    back - implemented logging and javadocs

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/service/impl/TenantServiceImpl.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/service/impl/TenantServiceImpl.java
@@ -5,10 +5,12 @@ import com.codewithomarm.rosterup.exceptions.DuplicateSubdomainException;
 import com.codewithomarm.rosterup.exceptions.InvalidTenantParameterException;
 import com.codewithomarm.rosterup.exceptions.TenantNotFoundException;
 import com.codewithomarm.rosterup.mapper.TenantMapper;
-import com.codewithomarm.rosterup.model.entity.Tenant;
+import com.codewithomarm.rosterup.model.Tenant;
 import com.codewithomarm.rosterup.repository.TenantRepository;
 import com.codewithomarm.rosterup.service.ITenantService;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TenantServiceImpl implements ITenantService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TenantServiceImpl.class);
 
     private final TenantRepository tenantRepository;
     private final TenantMapper tenantMapper;
@@ -28,43 +32,46 @@ public class TenantServiceImpl implements ITenantService {
 
     /**
      * Retrieves a page list of all tenants
+     *
      * @param pageable Pagination information
      * @return A page of TenantDTOs
      * @author Omar Montoya @codewithomarm
      */
     @Override
     public Page<TenantDTO> getAllTenants(Pageable pageable) {
-        return tenantRepository.findAll(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieving all tenants with pagination: {}", pageable);
+        Page<TenantDTO> allTenants = tenantRepository.findAll(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieved {} tenants", allTenants.getTotalElements());
+        return allTenants;
     }
 
     /**
      * Retrieves a tenant by its ID.
-     * @param tenantId the ID of the tenant to retrieve
+     *
+     * @param id the ID of the tenant to retrieve
      * @return the TenantDTO of the found tenant
      * @throws TenantNotFoundException if the tenant is not found
      * @throws InvalidTenantParameterException if the tenant id is empty or less than 0
      * @author Omar Montoya @codewithomarm
      */
     @Override
-    public TenantDTO getTenantById(String tenantId) {
-        // Parse tenant id from String to Long
-        Long id;
-        try {
-            id = Long.parseLong(tenantId);
-            if (id <= 0) {
-                throw new InvalidTenantParameterException("tenant id", "cannot be negative or zero");
-            }
-        } catch(NumberFormatException e) {
-            throw new InvalidTenantParameterException("tenant id", "needs to be numeric");
-        }
+    public TenantDTO getTenantById(String id) {
+        logger.info("Attempting to retrieve tenant with ID: {}", id);
+        Long tenantId = validateAndParseTenantId(id);
 
-        return tenantRepository.findById(id)
+        TenantDTO tenant = tenantRepository.findById(tenantId)
                 .map(tenantMapper::toDto)
-                .orElseThrow(() -> new TenantNotFoundException(tenantId));
+                .orElseThrow(() -> {
+                    logger.error("Tenant not found with ID: {}", tenantId);
+                    return new TenantNotFoundException(tenantId);
+                });
+        logger.info("Retrieved tenant with ID: {}", tenantId);
+        return tenant;
     }
 
     /**
      * Retrieves a page list of tenantDTO by name.
+     *
      * @param name the name of the tenants to retrieve
      * @param pageable Pagination information
      * @return a page of TenantDTOs
@@ -72,11 +79,15 @@ public class TenantServiceImpl implements ITenantService {
      */
     @Override
     public Page<TenantDTO> getTenantsByName(String name, Pageable pageable) {
-        return tenantRepository.findByName(name, pageable).map(tenantMapper::toDto);
+        logger.info("Retrieving tenants by name: {} with pagination: {}", name, pageable);
+        Page<TenantDTO> tenantsByName = tenantRepository.findByName(name, pageable).map(tenantMapper::toDto);
+        logger.info("Retrieved {} tenants with name: {}", tenantsByName.getTotalElements(), name);
+        return tenantsByName;
     }
 
     /**
      * Retrieves a tenant by its subdomain
+     *
      * @param subdomain the subdomain of the tenant to retrieve
      * @return the TenantDTO of the found tenant
      * @throws TenantNotFoundException if the tenant is not found
@@ -84,35 +95,50 @@ public class TenantServiceImpl implements ITenantService {
      */
     @Override
     public TenantDTO getTenantBySubdomain(String subdomain) {
-        return tenantRepository.findBySubdomain(subdomain)
+        logger.info("Attempting to retrieve tenant with subdomain: {}", subdomain);
+        TenantDTO tenant = tenantRepository.findBySubdomain(subdomain)
                 .map(tenantMapper::toDto)
-                .orElseThrow(() -> new TenantNotFoundException(subdomain));
+                .orElseThrow(() -> {
+                    logger.error("Tenant not found with subdomain: {}", subdomain);
+                    return new TenantNotFoundException(subdomain);
+                });
+        logger.info("Retrieved tenant with subdomain: {}", subdomain);
+        return tenant;
     }
 
     /**
      * Retrieves a paginated list of all active tenants.
-     * @param pageable Pagination information: page number, page size, and sorting details.
+     *
+     * @param pageable Pagination information.
      * @return a page containing tenantDTOs of active tenants.
      * @author Omar Montoya @codewithomarm
      */
     @Override
     public Page<TenantDTO> getActiveTenants(Pageable pageable) {
-        return tenantRepository.findAllActive(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieving active tenants with pagination: {}", pageable);
+        Page<TenantDTO> activeTenants = tenantRepository.findAllActive(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieved {} active tenants", activeTenants.getTotalElements());
+        return activeTenants;
     }
 
     /**
      * Retrieves a paginated list of all inactive tenants.
-     * @param pageable Pagination information: page number, page size, and sorting details.
+     *
+     * @param pageable Pagination information.
      * @return a page containing tenantDTOs of inactive tenants.
      * @author Omar Montoya @codewithomarm
      */
     @Override
     public Page<TenantDTO> getInactiveTenants(Pageable pageable) {
-        return tenantRepository.findAllInactive(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieving inactive tenants with pagination: {}", pageable);
+        Page<TenantDTO> inactiveTenants = tenantRepository.findAllInactive(pageable).map(tenantMapper::toDto);
+        logger.info("Retrieved {} inactive tenants", inactiveTenants.getTotalElements());
+        return inactiveTenants;
     }
 
     /**
      * Creates a new tenant.
+     *
      * @param tenantDto the tenantDTO containing the new tenant´s information.
      * @return the created TenantDTO.
      * @throws DuplicateSubdomainException if the subdomain already exists.
@@ -121,41 +147,49 @@ public class TenantServiceImpl implements ITenantService {
     @Override
     @Transactional
     public TenantDTO createTenant(TenantDTO tenantDto) {
-        // Validate if subdomain already exist
+        logger.info("Attempting to create a new tenant with subdomain: {}", tenantDto.getSubdomain());
         if (tenantRepository.findBySubdomain(tenantDto.getSubdomain()).isPresent()) {
+            logger.error("Subdomain already exists: {}", tenantDto.getSubdomain());
             throw new DuplicateSubdomainException(tenantDto.getSubdomain());
         }
         // Convert from Tenant DTO to Tenant Entity
         Tenant tenantEntity = tenantMapper.toEntity(tenantDto);
-
         // Set isActive value by default
         tenantEntity.setActive(true);
-
         // Save tenant entity in db
         Tenant savedTenantEntity = tenantRepository.save(tenantEntity);
-
         // Convert saved tenant entity to dto and return
+        logger.info("Successfully created new tenant with ID: {}", savedTenantEntity.getId());
         return tenantMapper.toDto(savedTenantEntity);
     }
 
     /**
      * Updates and existing tenant.
-     * @param tenantId the id from the tenant to be updated.
+     *
+     * @param id the id from the tenant to be updated.
      * @param tenantDto the tenantDTO containing the updated tenant´s information.
      * @return the updated tenantDTO.
      * @throws TenantNotFoundException if the tenant is not found based on the id provided.
+     * @throws DuplicateSubdomainException if the new subdomain is already in use by another tenant
      * @author Omar Montoya @codewithomarm
      */
     @Override
     @Transactional
-    public TenantDTO updateTenant(Long tenantId, TenantDTO tenantDto) {
+    public TenantDTO updateTenant(String id, TenantDTO tenantDto) {
+        logger.info("Attempting to update tenant with ID: {}", id);
+        Long tenantId = validateAndParseTenantId(id);
+
         // Fetch the existing tenant from the db using id parameter
         Tenant tenantEntity = tenantRepository.findById(tenantId)
-                .orElseThrow(() -> new TenantNotFoundException(tenantId));
+                .orElseThrow(() -> {
+                    logger.error("Tenant not found with ID: {}", tenantId);
+                    return new TenantNotFoundException(tenantId);
+                });
 
         // Verify if the subdomain is being changed and if it's already in use by other tenant
         if (!tenantEntity.getSubdomain().equals(tenantDto.getSubdomain()) &&
             tenantRepository.findBySubdomain(tenantDto.getSubdomain()).isPresent()) {
+            logger.error("Subdomain already in use: {}", tenantDto.getSubdomain());
             throw new DuplicateSubdomainException(tenantDto.getSubdomain());
         }
 
@@ -168,20 +202,49 @@ public class TenantServiceImpl implements ITenantService {
         Tenant updatedTenantEntity = tenantRepository.save(tenantEntity);
 
         // Convert and return updated tenant entity to dto
+        logger.info("Successfully updated tenant with ID: {}", updatedTenantEntity.getId());
         return tenantMapper.toDto(updatedTenantEntity);
     }
 
     /**
      * Deletes an existing tenant.
-     * @param tenantId the id from the tenant to be deleted.
+     *
+     * @param id the id from the tenant to be deleted.
      * @throws TenantNotFoundException if the tenant is not found based on the id provided.
      * @author Omar Montoya @codewithomarm
      */
     @Override
-    public void deleteTenant(Long tenantId) {
+    public void deleteTenant(String id) {
+        logger.info("Attempting to delete tenant with ID: {}", id);
+        Long tenantId = validateAndParseTenantId(id);
+
         tenantRepository.findById(tenantId)
-                .ifPresentOrElse(tenantRepository::delete, () -> {
+                .ifPresentOrElse(tenant -> {
+                    tenantRepository.delete(tenant);
+                    logger.info("Successfully deleted tenant with ID: {}", tenantId);
+                }, () -> {
+                    logger.error("Tenant not found with ID: {}", tenantId);
                     throw new TenantNotFoundException(tenantId);
                 });
+    }
+
+    /**
+     * Validates and parses the tenant ID from String to Long
+     * @param id the ID of the tenant as a String
+     * @return the parse Long ID
+     * @throws InvalidTenantParameterException if the ID is invalid
+     */
+    private Long validateAndParseTenantId(String id) {
+        try {
+            Long tenantId = Long.parseLong(id);
+            if (tenantId <= 0) {
+                logger.error("Invalid tenant ID: {}", id);
+                throw new InvalidTenantParameterException("tenant id", "cannot be negative or zero");
+            }
+            return tenantId;
+        } catch(NumberFormatException e) {
+            logger.error("Invalid tenant ID format: {}", id);
+            throw new InvalidTenantParameterException("tenant id", "needs to be numeric");
+        }
     }
 }
