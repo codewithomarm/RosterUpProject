74a9efac0b852145618755e68a0286a2ee5f5ec1
codewithomarm <codewithomarm@gmail.com>
2024-10-01 14:07:54 -0500

    back - implemented HATEOS in all controllers

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/controller/TenantController.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/controller/TenantController.java
@@ -1,11 +1,17 @@
 package com.codewithomarm.rosterup.controller;
 
-import com.codewithomarm.rosterup.dto.TenantDTO;
+import com.codewithomarm.rosterup.assembler.TenantPagedResourcesAssembler;
+import com.codewithomarm.rosterup.dto.request.tenant.CreateTenantRequest;
+import com.codewithomarm.rosterup.dto.request.tenant.UpdateTenantRequest;
+import com.codewithomarm.rosterup.dto.response.TenantResponse;
 import com.codewithomarm.rosterup.service.ITenantService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,42 +23,56 @@ import java.net.URI;
 public class TenantController {
 
     private final ITenantService tenantService;
+    private final TenantPagedResourcesAssembler tenantPagedResourcesAssembler;
 
     @Autowired
-    public TenantController(ITenantService tenantService) {
+    public TenantController(ITenantService tenantService, TenantPagedResourcesAssembler tenantPagedResourcesAssembler) {
         this.tenantService = tenantService;
+        this.tenantPagedResourcesAssembler = tenantPagedResourcesAssembler;
     }
 
     @GetMapping() // GET http://localhost:8080/api/roster-up/tenants
-    public ResponseEntity<Page<TenantDTO>> getAllTenants(Pageable pageable) {
-        Page<TenantDTO> tenants = tenantService.getAllTenants(pageable);
-        return ResponseEntity.ok(tenants);
+    public ResponseEntity<PagedModel<EntityModel<TenantResponse>>> getAllTenants(Pageable pageable) {
+        Page<TenantResponse> allTenantsPagedResponse = tenantService.getAllTenants(pageable);
+
+        PagedModel<EntityModel<TenantResponse>> allTenantsPagedModel = tenantPagedResourcesAssembler.toPagedModel(allTenantsPagedResponse);
+
+        return ResponseEntity.ok(allTenantsPagedModel);
     }
 
     @GetMapping("/{id}") // GET http://localhost:8080/api/roster-up/tenants/{id}
-    public ResponseEntity<TenantDTO> getTenantById(@PathVariable String id) {
-        TenantDTO tenantDTO = tenantService.getTenantById(id);
-        return ResponseEntity.ok(tenantDTO);
+    public ResponseEntity<EntityModel<TenantResponse>> getTenantById(@PathVariable String id) {
+        TenantResponse tenantByIdResponse = tenantService.getTenantById(id);
+
+        EntityModel<TenantResponse> tenantByIdModel = tenantPagedResourcesAssembler.toModel(tenantByIdResponse);
+
+        return ResponseEntity.ok(tenantByIdModel);
     }
 
     @PostMapping() // POST http://localhost:8080/api/roster-up/tenants
-    public ResponseEntity<TenantDTO> createTenant(@Valid @RequestBody TenantDTO tenantDTO) {
-        TenantDTO createdTenant = tenantService.createTenant(tenantDTO);
+    public ResponseEntity<EntityModel<TenantResponse>> createTenant(@Valid @RequestBody CreateTenantRequest request) {
+        TenantResponse createdTenantResponse = tenantService.createTenant(request);
 
         // Set the location header for the newly created tenant
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(createdTenant.getId())
+                .buildAndExpand(createdTenantResponse.getId())
                 .toUri();
 
-        return ResponseEntity.created(location).body(createdTenant);
+        EntityModel<TenantResponse> createdTenantModel = tenantPagedResourcesAssembler.toModel(createdTenantResponse);
+
+        return ResponseEntity.created(location).body(createdTenantModel);
     }
 
     @PutMapping("/{id}") // PUT http://localhost:8080/api/roster-up/tenants/{id}
-    public ResponseEntity<TenantDTO> updateTenant(@PathVariable String id, @Valid @RequestBody TenantDTO tenantDTO) {
-        TenantDTO updatedTenant = tenantService.updateTenant(id, tenantDTO);
-        return ResponseEntity.ok(updatedTenant);
+    public ResponseEntity<EntityModel<TenantResponse>> updateTenant(@PathVariable String id,
+                                                                    @Valid @RequestBody UpdateTenantRequest request) {
+        TenantResponse updatedTenantResponse = tenantService.updateTenant(id, request);
+
+        EntityModel<TenantResponse> updatedTenantModel = tenantPagedResourcesAssembler.toModel(updatedTenantResponse);
+
+        return ResponseEntity.ok(updatedTenantModel);
     }
 
     @DeleteMapping("/{id}") // DELETE http://localhost:8080/api/roster-up/tenants/{id}
@@ -62,26 +82,38 @@ public class TenantController {
     }
 
     @GetMapping("/search/name") // GET http://localhost:8080/api/roster-up/tenants/search/name?name=tenantName
-    public ResponseEntity<Page<TenantDTO>> getTenantsByName(@RequestParam String name, Pageable pageable) {
-        Page<TenantDTO> tenants = tenantService.getTenantsByName(name, pageable);
-        return ResponseEntity.ok(tenants);
+    public ResponseEntity<PagedModel<EntityModel<TenantResponse>>> getTenantsByName(@RequestParam String name,
+                                                                                    @PageableDefault(size = 20) Pageable pageable) {
+        Page<TenantResponse> tenantsByNameResponse = tenantService.getTenantsByName(name, pageable);
+
+        PagedModel<EntityModel<TenantResponse>> tenantsByNamePagedModel = tenantPagedResourcesAssembler.toPagedModelWithNameSearch(tenantsByNameResponse, name);
+
+        return ResponseEntity.ok(tenantsByNamePagedModel);
     }
 
-    @GetMapping("7search/active") // GET http://localhost:8080/api/roster-up/tenants/search/active?active={true/false}
-    public ResponseEntity<Page<TenantDTO>> getTenantsByActiveStatus(
-            @RequestParam(name="active", required=true) Boolean isActive, Pageable pageable) {
-        Page<TenantDTO> tenants;
+    @GetMapping("/search/active") // GET http://localhost:8080/api/roster-up/tenants/search/active?active={true/false}
+    public ResponseEntity<PagedModel<EntityModel<TenantResponse>>> getTenantsByActiveStatus(
+            @RequestParam(name="active", required=true) Boolean isActive,
+            @PageableDefault(size = 20) Pageable pageable) {
+        Page<TenantResponse> tenantsByActiveStatusResponse;
         if (isActive) {
-            tenants = tenantService.getActiveTenants(pageable);
+            tenantsByActiveStatusResponse = tenantService.getActiveTenants(pageable);
         } else {
-            tenants = tenantService.getInactiveTenants(pageable);
+            tenantsByActiveStatusResponse = tenantService.getInactiveTenants(pageable);
         }
-        return ResponseEntity.ok(tenants);
+
+        PagedModel<EntityModel<TenantResponse>> tenantsByActiveStatusPagedModel = tenantPagedResourcesAssembler
+                .toPagedModelWithActiveStatus(tenantsByActiveStatusResponse, isActive);
+
+        return ResponseEntity.ok(tenantsByActiveStatusPagedModel);
     }
 
     @GetMapping("/subdomains/{subdomainName}") // GET http://localhost:8080/api/roster-up/tenants/subdomains/{subdomainName}
-    public ResponseEntity<TenantDTO> getTenantBySubdomain(@PathVariable String subdomainName) {
-        TenantDTO tenantDTO = tenantService.getTenantBySubdomain(subdomainName);
-        return ResponseEntity.ok(tenantDTO);
+    public ResponseEntity<EntityModel<TenantResponse>> getTenantBySubdomain(@PathVariable String subdomainName) {
+        TenantResponse tenantBySubdomainResponse = tenantService.getTenantBySubdomain(subdomainName);
+
+        EntityModel<TenantResponse> tenantBySubdomainModel = tenantPagedResourcesAssembler.toModel(tenantBySubdomainResponse);
+
+        return ResponseEntity.ok(tenantBySubdomainModel);
     }
 }
