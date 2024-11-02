6fa374cb4f945a37bd351c8aadbe642cf386a272
codewithomarm <codewithomarm@gmail.com>
2024-11-02 09:53:05 -0500

    back - updated user controller. wip

--- root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/security/v1/user/controller/RosterupUserControllerV1.java
+++ root/backend/rosterup/src/main/java/com/codewithomarm/rosterup/security/v1/user/controller/RosterupUserControllerV1.java
@@ -1,4 +1,32 @@
 package com.codewithomarm.rosterup.security.v1.user.controller;
 
+import com.codewithomarm.rosterup.security.v1.user.assembler.RosterupUserPagedResourceAssembler;
+import com.codewithomarm.rosterup.security.v1.user.dto.response.RosterupUserResponse;
+import com.codewithomarm.rosterup.security.v1.user.service.IRosterupUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/roster-up/v1/users")
 public class RosterupUserControllerV1 {
+
+    private final IRosterupUserService rosterupUserService;
+    private final RosterupUserPagedResourceAssembler rosterupUserPagedResourceAssembler;
+
+    @Autowired
+    public RosterupUserControllerV1(IRosterupUserService rosterupUserService,
+                                    RosterupUserPagedResourceAssembler rosterupUserPagedResourceAssembler) {
+        this.rosterupUserService = rosterupUserService;
+        this.rosterupUserPagedResourceAssembler = rosterupUserPagedResourceAssembler;
+    }
+
+
+    public ResponseEntity<EntityModel<RosterupUserResponse>> getUserById(@PathVariable String userId) {
+        //return ResponseEntity.ok(rosterupUserPagedResourceAssembler.toModel(rosterupUserService.getUserById(userId)));
+        return null;
+    }
 }
